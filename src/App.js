fac722ef5117f458915366ec41865636d9a7b44c
Daniel Zen <daniel@zen.digital>
2017-03-08 05:58:16 -0500

    master

--- src/App.js
+++ src/App.js
@@ -5,26 +5,56 @@ import {
   Link
 } from 'react-router-dom';
 
-const Child = ({ match }) => (
+const Home = () => (
   <div>
-    <h3>ID: {match.params.id}</h3>
+    <h2>Home</h2>
   </div>
 );
 
-const ParamsExample = () => (
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li><Link to={`${match.url}/rendering`}>Rendering with React</Link></li>
+      <li><Link to={`${match.url}/components`}>Components</Link></li>
+      <li><Link to={`${match.url}/props-v-state`}>Props v. State</Link></li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+);
+
+const BasicExample = () => (
   <Router>
     <div>
-      <h2>Accounts</h2>
       <ul>
-        <li><Link to="/netflix">Netflix</Link></li>
-        <li><Link to="/zillow-group">Zillow Group</Link></li>
-        <li><Link to="/yahoo">Yahoo</Link></li>
-        <li><Link to="/modus-create">Modus Create</Link></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
       </ul>
 
-      <Route path="/:id" component={Child}/>
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/topics" component={Topics}/>
     </div>
   </Router>
 );
 
-export default ParamsExample;
+export default BasicExample;
